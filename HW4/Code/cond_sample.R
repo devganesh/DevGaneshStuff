f81b8548e14e71c9f7c359ed942014a94a222c7e
Dev Ganesh <ganesh@ucdavis.edu>
2013-12-12 13:47:45 -0800

    Code cleaned

--- HW4/Code/cond_sample.R
+++ HW4/Code/cond_sample.R
@@ -3,7 +3,7 @@ library(MASS)
 library(truncnorm)
 
 #function samples from the posterior beta|Z
-cond_sample <- function(Z, X, XX, beta_0, sigma_0_inv ){
+cond_sample <-function(Z, X, XX, beta_0, sigma_0_inv ){
   ZX = colSums(Z*X)
   Sigma = solve(sigma_0_inv + XX)
   Mu = Sigma%*%(sigma_0_inv%*%beta_0 + ZX)
